c87750872bd89c7e69962bd597bc6eada4a46502
Athena Boose <pestpestthechicken@yahoo.com>
2025-03-10 22:00:31 -0700

    Wrote buffer, now to write tests.

---
+++ src/buffer.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "buffer.h"
+
+
+static size_t min_buf_size(size_t data_len) {
+    if (data_len == 0) {
+        return 0;
+    }
+
+    size_t result = 1;
+    while (data_len != 0) {
+        data_len <<= 1;
+        result >>= 1;
+    }
+    return result;
+}
+
+void *buffer_reserve(struct buffer *buf, size_t bytes_to_reserve) {
+    if (buf->alloc_start == NULL) {
+        // this is a new buffer
+        buf->capacity = min_buf_size(bytes_to_reserve);
+        buf->alloc_start = malloc(buf->capacity);
+        return buf->alloc_start;
+    }
+
+    if (buf->start_position == 0 && buf->capacity - buf->end_position > bytes_to_reserve) {
+        // enough space in buffer already
+        return (char *)buf->alloc_start + buf->end_position;
+    }
+
+    // this might be able to be optimized with calls to realloc() instead of malloc() / memcpy()
+
+    size_t temp_capacity = min_buf_size(bytes_to_reserve);
+    void *temp = malloc(temp_capacity);
+
+    if (temp != NULL) {
+        memcpy(temp, (char *)buf->alloc_start + buf->start_position, buf->end_position - buf->start_position);
+        free(buf->alloc_start);
+        buf->alloc_start = temp;
+        buf->capacity = temp_capacity;
+
+        temp = (char *)buf->alloc_start + buf->end_position; // stupid
+    }
+    return temp;
+}
+
+void buffer_write(struct buffer *buf, size_t bytes_written) {
+    buf->end_position += bytes_written;
+}
+
+void *buffer_start(struct buffer *buf) {
+    return (char *)buf->alloc_start + buf->start_position;
+}
+
+size_t buffer_bytes_readable(struct buffer *buf) {
+    return buf->end_position - buf->start_position;
+}
+
+void buffer_read(struct buffer *buf, size_t bytes_read) {
+    buf->start_position += bytes_read;
+}
+
+void buffer_new(struct buffer *buf) {
+    buf->alloc_start = NULL;
+    buf->start_position = 0;
+    buf->end_position = 0;
+    buf->capacity = 0;
+}
+
+void buffer_free(struct buffer *buf) {
+    if (buf->alloc_start != NULL) {
+        free(buf->alloc_start);
+        buf->alloc_start = NULL;
+    }
+    buf->start_position = 0;
+    buf->end_position = 0;
+    buf->capacity = 0;
+}
---
+++ src/buffer.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <stddef.h>
+
+struct buffer {
+    // private 
+    void *alloc_start; 
+    size_t start_position;
+    size_t end_position;
+    size_t capacity;
+};
+
+
+/*
+ * Ensures that at least `bytes_to_reserve` bytes can be written to the buffer pointed to by `buf`.
+ *
+ * Returns a pointer to the first uninitialized byte in the buffer.
+ * You'll want to pass this pointer to read(2) or whatever.
+ *
+ * This pointer may be NULL if malloc(3) fails.
+ * If this pointer is NULL, you **still need to free the buffer** with a call to buffer_free().
+ */
+void *buffer_reserve(struct buffer *buf, size_t bytes_to_reserve);
+
+/*
+ * Moves the end position of the buffer `bytes_written` bytes further along.
+ * It is undefined behavior to call this function without first reserving at least `bytes_written` bytes.
+ *
+ * It's intended to call this function after calling read(2) or similar.
+ * read(2) reads some number of bytes into the pointer given to you by `buffer_reserve`, and then you can call this function to advance the buffer that number of bytes.
+ */
+void buffer_write(struct buffer *buf, size_t bytes_written);
+
+/*
+ * Gets a pointer to the start of the buffer.
+ * If the buffer is a new buffer and nothing has been written to it, NULL will be returned. Otherwise, it is guaranteed that NULL will not be returned.
+ */
+void *buffer_start(struct buffer *buf);
+
+/*
+ * Returns the number of bytes that are in the buffer available to be read.
+ */
+size_t buffer_bytes_readable(struct buffer *buf);
+
+/*
+ * Moves the start position of the buffer `bytes_read` bytes further along.
+ * Effectively, this frees the first `bytes_read` bytes of the buffer.
+ * It is undefined to access these bytes after this function is called.
+ */
+void buffer_read(struct buffer *buf, size_t bytes_read);
+
+/*
+ * Initializes a new buffer at the location pointed to by `buf`.
+ * This function does not itself allocate any memory and thus is guaranteed to succeed.
+ */
+void buffer_new(struct buffer *buf);
+
+/*
+ * Frees the buffer pointed to by `buf`.
+ * `buf` will be set to the same state as a buffer created by buffer_new.
+ */
+void buffer_free(struct buffer *buf);
+
