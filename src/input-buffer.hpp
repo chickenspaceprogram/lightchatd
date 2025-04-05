09c35b28dad766d624ea4b3ef49ec8f1a2315ebb
Athena Boose <pestpestthechicken@yahoo.com>
2025-04-04 21:26:32 -0700

    Added a ring buffer that should serve well as an input buffer.
    Tests have not been created yet, those will come in a future
    commit.

---
+++ src/input-buffer.hpp
@@ -0,0 +1,77 @@
+#pragma once
+#include <cstddef>
+#include <event2/event.h>
+#include <array>
+#include <sys/uio.h>
+
+template <size_t buffer_size>
+class InputBuffer {
+    private:
+    static constexpr inline bool is_power_of_two(std::size_t val) {
+        return val && !(val & (val - 1)); // see http://www.graphics.stanford.edu/~seander/bithacks.html
+    }
+
+    static_assert(is_power_of_two(buffer_size), "The size of a RingBuffer must be a power of 2."); // this is premature optimization but i love bitwise operators :3
+    public:
+    constexpr InputBuffer() noexcept : buffer{}, buffer_full(false), start(0), end(0) {}
+
+    constexpr InputBuffer(InputBuffer &&buf) = default;
+    constexpr InputBuffer &operator=(InputBuffer &&buf) = default;
+
+    constexpr char &operator[](const size_t index) noexcept { return buffer[mod(start + index)]; }
+    constexpr const char &operator[](const size_t index) const noexcept { return buffer[mod(start + index)]; }
+
+    constexpr size_t size() const noexcept {
+        if (buffer_full) {
+            return buffer_size;
+        }
+        return (end >= start) ? (end - start) : (buffer_size - (end - start));
+    }
+    constexpr bool empty() const noexcept { return !buffer_full && start == end; }
+    constexpr bool full() const noexcept { return buffer_full; }
+
+    ssize_t append_read(evutil_socket_t fd) noexcept {
+        if (buffer_full) {
+            return 0;
+        }
+        if (end < start || start == 0) {
+            ssize_t retval = read(fd, buffer.data() + end, start - end);
+            if (retval == -1 || retval == 0) {
+                return retval;
+            }
+            end += retval;
+            if (start == end) {
+                buffer_full = true;
+            }
+        }
+        else {
+            std::array<iovec, 2> iovecs = {
+                iovec { .iov_base = buffer.data() + end, .iov_len = buffer_size - end },
+                iovec { .iov_base = buffer.data(), .iov_len = start } 
+            };
+            ssize_t retval = readv(fd, iovecs.data(), 2);
+            if (retval == -1 || retval == 0) {
+                return retval;
+            }
+            end = mod(end + retval);
+            if (start == end) {
+                buffer_full = true;
+            }
+        }
+    }
+
+    constexpr void remove_front(size_t amount) noexcept {
+        start = mod(start + amount);
+    }
+
+    private:
+    constexpr InputBuffer(const InputBuffer &) = default;
+    constexpr InputBuffer &operator=(const InputBuffer &) = default;
+
+    constexpr static size_t mod(const size_t val) noexcept { return val & (buffer_size - 1); } // again, premature optimization
+
+    std::array<char, buffer_size> buffer;
+    bool buffer_full;
+    size_t start;
+    size_t end;
+};
